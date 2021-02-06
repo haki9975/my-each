ba143e422415e8d8577f4ecbe56ff6ef5438f661
haki9975 <elias.robert.hakim@gmail.com>
2021-02-06 14:29:33 -0500

    commit tree 2ed1cd379466157112b514635f3d106bc8fb8cb0

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,20 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+   if block_given? 
+    i = 0
+    while i < array.length
+      yield(array[i]) 
+       i = i + 1
+    end
+     array
+  else 
+    p "There is no block given"
+  end
 end
+
+  # calls on while
+  # iterates
+  # yeilds correct element
+  # can handle empty array
+  # return original array
+  # block is run "n" times
+  # only passes single element into the block at a time
